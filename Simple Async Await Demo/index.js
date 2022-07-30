49433d4bd7632c72d69cba12f4e4fcffea3bc4ac
nikhiltrash <nikhiltrash23@gmail.com>
2022-07-30 12:21:26 +0000

    RxJS Crash Course

---
+++ Simple Async Await Demo/index.js
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+// Example POST method implementation:
+console.log('Start');
+let url = 'https://jsonplaceholder.typicode.com/users';
+async function postData() {
+  console.log('Fetch');
+  const response = await fetch(url);
+  console.log('Await');
+  const data = await response.json();
+  console.log('Response Received');
+  return data;
+}
+async function getData() {
+  console.log('Get Data');
+  let receivedData = await postData();
+  console.log('Data Received');
+  console.log(receivedData);
+}
+
+getData();
+console.log('End');
